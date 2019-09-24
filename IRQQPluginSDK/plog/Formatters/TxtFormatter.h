ce83e2ecf6c32d3d7418d66c75398ff3346ef03c
mengdejun <mengdj@outlook.com>
2019-09-24 15:27:06 +0800

    1.1.6

--- IRQQPluginSDK/plog/Formatters/TxtFormatter.h
+++ IRQQPluginSDK/plog/Formatters/TxtFormatter.h
@@ -24,7 +24,7 @@ namespace plog
             ss << std::setfill(PLOG_NSTR(' ')) << std::setw(5) << std::left << severityToString(record.getSeverity()) << PLOG_NSTR(" ");
             ss << PLOG_NSTR("[") << record.getTid() << PLOG_NSTR("] ");
             ss << PLOG_NSTR("[") << record.getFunc() << PLOG_NSTR("@") << record.getLine() << PLOG_NSTR("] ");
-            ss << record.getMessage() << PLOG_NSTR("\n");
+            ss << record.getMessage() << PLOG_NSTR("\r\n");
 
             return ss.str();
         }
